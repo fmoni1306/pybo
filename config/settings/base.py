a32af5e85fcfce39619afed074f60eb60d8a5097
이태윤 <yoon@itaeyun-ui-MacBookPro.local>
2021-03-30 16:51:50 +0900

    pybo 로거 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -172,7 +172,7 @@ LOGGING = {
             'style': '{',
         },
         'standard': {
-            # asctime = 현재시간 , levelname - 로그의 레벨(debud, info, wrning, error, critical)
+            # asctime = 현재시간 , levelname - 로그의 레벨(debug, info, wrning, error, critical)
             # name - 로거명
             # message - 출력내용
             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
@@ -194,16 +194,16 @@ LOGGING = {
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
-        """
-         level = 출력 레벨로 INFO 를 사용
-         filters - DEBUG =False 인 운영환경에 사용
-         class - 파일 핸들러로 RotatingFileHandler 사용, RotatingFileHandler는 파일 크기가 설정한크기보다 커지면 파일 뒤에 인덱스를 붙여서 백업한다. 
-         이 핸들러의 장점은 로그가 무한히 증가되더라도 일정 개수의 파일로 롤릴 되기 때문에 로그 파일이 너무 커져서 디스크가 꽉차는 위험을 방지할 수 있다.
-         filename - 로그 파일명은 ㅣogs 디렉터리에 mysite.log로 설정
-         maxBytes - 로그 파일 크기는 5MB로 설정
-         backupCount - 롤링되는 파일 개수를 의미한다. 총 5개의 로그 파일로 유지되도록 설정했다.
-         formatter - 포맷터는 standard를 사용
-        """
+
+        # level = 출력 레벨로 INFO 를 사용
+        # filters - DEBUG =False 인 운영환경에 사용
+        # class - 파일 핸들러로 RotatingFileHandler 사용, RotatingFileHandler는 파일 크기가 설정한크기보다 커지면 파일 뒤에 인덱스를 붙여서 백업한다.
+        # 이 핸들러의 장점은 로그가 무한히 증가되더라도 일정 개수의 파일로 롤릴 되기 때문에 로그 파일이 너무 커져서 디스크가 꽉차는 위험을 방지할 수 있다.
+        # filename - 로그 파일명은 ㅣogs 디렉터리에 mysite.log로 설정
+        # maxBytes - 로그 파일 크기는 5MB로 설정
+        # backupCount - 롤링되는 파일 개수를 의미한다. 총 5개의 로그 파일로 유지되도록 설정했다.
+        # formatter - 포맷터는 standard를 사용
+
         'file': {
             'level': 'INFO',
             'filters': ['require_debug_false'],
@@ -224,6 +224,11 @@ LOGGING = {
             'handlers': ['django.server'],
             'level': 'INFO',
             'propagate': False,
+        },
+        'pybo': {
+            'handler': ['console', 'file'],
+            'level': 'INFO',
+
         },
     }
 }
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,12 +2,17 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, Count
 from ..models import Question, Answer
+import logging
+
+logger = logging.getLogger('pybo')
 
 
 def index(request):
     """
             pybo 목록 출력
     """
+    logger.info("INFO 레벨로 출력")
+
     # 입력 인자
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
