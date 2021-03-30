2437ea99b229cbf3c601b49e900b16bdb3dd739e
yoon <fmoni1306@gmail.com>
2021-03-30 17:37:54 +0900

    Default Changelist

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,17 @@ ALLOWED_HOSTS = ['3.34.8.110']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+
+# PostgreSQL 적용
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '6q}Z^TVHBG1_aqm`~OjeS+{h~oS7<1hW',
+        'HOST': 'ls-31f7340c3549d2c2ab46f29bee6301ef8c0f67db.cx18kr6969b0.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
